35fca3f6c5e6350e1d60e8ad674fb77feee07ee5
Mirojane <janelvjuan@hotmail.com>
2018-07-27 09:29:15 +0800

    commit Rooster class

---
+++ src/com/singtel/animals/Rooster.java
@@ -0,0 +1,26 @@
+package com.singtel.animals;
+
+// For question A-3
+public class Rooster extends Chicken {
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		super.fly();
+	}
+
+	@Override
+	public void walk() {
+		// TODO Auto-generated method stub
+		super.walk();
+	}
+
+	@Override
+	public void sing() {
+			super.sing();
+			System.out.println("I am rooster, Cock-a-doodle-doo");
+	}
+	
+	
+
+}
